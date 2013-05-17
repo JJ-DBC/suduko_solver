e1288aa6ffdb0541a8dcc4c7ba12384dfb333959
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-17 14:25:55 -0500

    we put in methods for initialize, is_solved, and print_board. does not contain searching methods yet

--- sudoku.rb
+++ sudoku.rb
@@ -1,8 +1,25 @@
 class Sudoku
   def initialize(board_string)
+    @board = []
+    board_string.split(//).each {|num| num.to_i}.each_slice(9) { |row| @board << row  }
   end
 
+  def is_solved?
+    @unsolved_count = @board.flatten.select { |num| num.to_i == 0}
+    if @unsolved_count == 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def print_board
+    @board.each { |row| puts row.join(" ") }
+  end
+
+
   def solve!
+
   end
 
   # Returns a string representing the current state of the board
@@ -13,6 +30,7 @@ class Sudoku
   end
 end
 
+
 # The file has newlines at the end of each line, so we call
 # String#chomp to remove them.
 board_string = File.readlines('sample.unsolved.txt').first.chomp
@@ -20,6 +38,10 @@ board_string = File.readlines('sample.unsolved.txt').first.chomp
 game = Sudoku.new(board_string)
 
 # Remember: this will just fill out what it can and not "guess"
-game.solve!
+# game.solve!
 
-puts game.board
+# puts game.board
+
+#p game.is_solved?
+
+game.print_board
